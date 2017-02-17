2c9c8ac55d308813e4277f7d00306559392f1023
Acreddy <sreedhar.a14@iiits.in>
2017-02-17 22:40:10 +0530

    urls and views for new models

--- website/Transport/views.py
+++ website/Transport/views.py
@@ -6,49 +6,26 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from Transport.models import *
-# Create your views here.
 from Transport.forms import *
 
-def index(request):
-     if not request.user.is_authenticated():
-           return render(request, 'Transport/login.html')
-     else:
-           return render(request,'Transport/index.html')
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
 
-
-def place(request,name_id):
-    temp_veh = driver.objects.get(id=name_id)
-    temp_veh_liv = vehicle_live.objects.get(vehicle = temp_veh)
-    #if (temp_veh_liv == None):
-    #    temp_veh_liv = vehicle_live()
-    #    temp_veh_liv.vehicle = temp_veh
-    #    temp_veh_liv.lon_pos = 80.0091568
-    #    temp_veh_liv.lat_pos = 13.5481095
-    #    temp_veh.save()
-    a = {
-    "temp_veh_liv":temp_veh_liv,
-    }
-    return render(request,"Transport/place.html",a)
-def allvehicles(request):
-    vehicle_lives=vehicle_live.objects.all()
-    context={
-        "vehicle_lives":vehicle_lives,
-    }
-    return render(request,"Transport/allvehicles.html",context)
-def vehicle_details(request):
-    vehi = vehicle.objects.all()
+                return render(request, 'Transport/index.html')
     context = {
-     "vehi":vehi,
-     }
-    return render(request,"Transport/vehicle_details.html",context)
-
-
-def Booking(request):
-    form = BookingForm()
-    context = {
-    "form": form,
+        "form": form,
     }
-    return render(request,"Transport/Booking.html",context)
+    return render(request, 'Transport/register.html', context)
 
 def login_user(request):
     if request.method == "POST":
@@ -75,38 +52,63 @@ def logout_user(request):
     return render(request, 'Transport/login.html', context)
 
 
-def register(request):
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        user = form.save(commit=False)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password']
-        user.set_password(password)
-        user.save()
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
+def index(request):
+     if not request.user.is_authenticated():
+           return render(request, 'Transport/login.html')
+     else:
+           return render(request,'Transport/index.html')
 
-                return render(request, 'Transport/index.html')
+def allpassengers(request):                                     #displays all passengers
+    passengers = Passenger.objects.all()
+    context    = {
+        "passengers":passengers,
+    }
+    return render(request,"Transport/passengers.html",context)
+
+def alldrivers(request):                                        #display all drivers
+    drivers = Driver.objects.all()
+    count_Drivers = range(1,drivers.count()+1)
     context = {
-        "form": form,
+    "drivers":drivers,
+    "count_Drivers" : count_Drivers,
     }
-    return render(request, 'Transport/register.html', context)
+    return render(request, "Transport/drivers.html",context)
 
-def user_accounts(request):
-    temp_user_acc = My_user.objects.all()
-    usr_cont = {
-     "my_usr":temp_user_acc,
+def allvehicles(request):                                       #To display all drivers
+    vehicles = Vehicle.objects.all()                            #gets all vehicle details
+    context  = {                    
+        "vehicles":vehicles,
+    }
+    return render(request,"Transport/vehicles.html",context)
+
+
+def driver_details(request,name_id):                            #Details of driver with name_id as id
+    driver  = Driver.objects.get(id=name_id)
+    context = {
+    "driver" : driver,
+    }
+    return render(request,"Transport/driver.html",context)
+
+def vehicle_details(request,name_id):                           #Details of vehicle with id name_id
+    vehicle = Vehicle.objects.get(id=name_id)
+    context = {
+     "vehicle":vehicle,
      }
-    return render(request,"Transport/usr_acc.html",usr_cont)
+    return render(request,"Transport/vehicle_details.html",context)
 
-def driver_accounts(request):
-    temp_driver_acc = driver.objects.all()
-    num_records = range(1,temp_driver_acc.count()+1)
-    print num_records
-    driver_cont = {
-    "my_driv":temp_driver_acc,
-    "total" : num_records,
+def passenger_details(request,name_id):                         #Details of passenger with id name_id
+    passenger = Passenger.objects.get(id=name_id)
+    context   = {
+        "passenger":passenger
     }
-    return render(request, "Transport/driv_acc.html",driver_cont)
+    return render(request,"Transport/passenger_details.html",context)
+
+'''def Booking(request):
+    form = BookingForm()
+    context = {
+    "form": form,
+    }
+    return render(request,"Transport/Booking.html",context)'''
+
+
+
