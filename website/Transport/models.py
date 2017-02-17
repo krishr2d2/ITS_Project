995c5540463d78619dc0872437fece1b9e60389c
Acreddy <sreedhar.a14@iiits.in>
2017-02-17 22:03:58 +0530

    Added capital letter in the front

--- website/Transport/models.py
+++ website/Transport/models.py
@@ -18,41 +18,41 @@ class My_user(models.Model):
     My_user_email       = models.CharField(max_length=250)
     My_user_dateOfBirth = models.DateField()#To know the age of user
 
-class driver(models.Model):#stores all driver details
+class Driver(models.Model):#stores all driver details
     driver_My_user        = models.ForeignKey(My_user,on_delete:CASCADE)#to get all details from My user
     driver_currentVehicle = models.ForeignKey(vehicle,on_delete:CASCADE)
     driver_liscence       = models.CharField(max_length=20)
+    driver_longitude      = models.FloatField()#longitude of driver
+    driver_latitude       = models.FloatField()#latitude  of driver
     #driver_currentVehicle gets current detail of vehicle driver
 
-class owner(models.Model):#stores all owner details
-    owner_My_user = models.ForeignKey(My_user,on_delete:CASCADE)#to get all details from My user
-
-class passenger(models.Model):#stores all passangers details
+class Passenger(models.Model):#stores all passangers details
     passenger_My_user   = models.ForeignKey(My_user,on_delete:CASCADE)#to get all details from My user
     passenger_latitude  = models.FloatField()#current latitude of passenger
     passenger_longitude = models.FloatField()#current longitude of passenger
     passenger_active    = models.BooleanField()#
 
-class valid_stop(self):#This model stores all the valid stops
+class Valid_stop(self):#This model stores all the valid stops
     valid_stop_name      = models.CharField(max_length=50)#name of the stops
     valid_stop_latitude  = models.FloatField()#latitude of valid stops
     valid_stop_longitude = models.FloatField()#longitude of valid stops
 
-class booking(models.Model):#Stores all the information about bookings
+class Booking(models.Model):#Stores all the information about bookings
     booking_startpoint  = models.ForeignKey(valid_stop,on_delete:CASCADE)#starting point of booking
     booking_destination = models.ForeignKey(valid_stop,on_delete:CASCADE)#ending point of booking
-    booking_date        = models.DateTimeField()
+    booking_date        = models.DateTimeField(auto_add_now=True)
     booking_passanger   = models.ForeignKey(My_user, on_delete=models.CASCADE)#passanger
     booking_vehicle     = models.ForeignKey(vehicle,  on_delete=models.CASCADE)#vehicle booked
     booking_status      = models.CharField(max_length=50,default='pending')
-    booking_driver      = models.ForeignKey(driver,on_delete=models.CASCADE)#driver of the vehicle at the time of booking
+    #booking_driver      = models.ForeignKey(driver,on_delete=models.CASCADE)#driver of the vehicle at the time of booking
 
-class vehicle(models.Model):#Stores information about vehicles
+class Vehicle(models.Model):#Stores information about vehicles
     vehicle_name      = models.CharField(max_length=200)
     vehicle_latitude  = models.FloatField()#latitude of vehicle
     vehicle_longitude = models.FloatField()#longitude of vehicle
     vehicle_owner     = models.ForeignKey(owner,on_delete:CASCADE)#owner of the vehicle
     vehicle_rc        = models.CharField(max_length=200)#rc of the vehicle
+    vehicle_active    = models.BooleanField(default=False)#status of vehicle
 
     
 
